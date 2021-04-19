acb0c3fe219790875715a9b9705118ca18e60967
Luci Temple <lucitemple@yahoo.com.au>
2021-04-19 16:18:23 +1000

    Refactor courseinfo step 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -12,12 +12,12 @@ const Part = (props) => {
   );
 };
 
-const Content = (props) => {
+const Content = (props) => { 
   return (
     <>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </>
   );
 };
@@ -26,35 +26,33 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      {props.parts[0].exercises + props.parts[1].exercises  + props.parts[2].exercises }
     </p>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
+const parts = [
+  {
     name: "Fundamentals of React",
     exercises: 10,
-  };
-  const part2 = {
+  },
+  {
     name: "Using props to pass data",
     exercises: 7,
-  };
-  const part3 = {
+  },
+  {
     name: "State of a component",
     exercises: 14,
-  };
+  },
+];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };

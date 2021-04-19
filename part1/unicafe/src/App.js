96338f91d842f0ebd12b1a4cbc5da62c02317a32
Luci Temple <lucitemple@yahoo.com.au>
2021-04-20 00:38:07 +1000

    Complete 1.9 unicafe step 4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,17 +4,25 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
 
-const Statistics = (props) => (
-  <p>
-    {props.text} {props.statistics}
-  </p>
-);
+const Statistics = (props) => {
+  if (props.statistics === 0 || isNaN(props.statistics)) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <p>
+      {props.text} {props.statistics}
+      {props.append}
+    </p>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const average = () => (good - bad) / (good + bad + neutral);
 
   return (
     <div>
@@ -23,12 +31,18 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
       <h2>Statistics</h2>
-      <Statistics text="Good" statistics={good} />
-      <Statistics text="Neutral" statistics={neutral} />
-      <Statistics text="Bad" statistics={bad} />
-      <Statistics text="Reviews" statistics={bad + neutral + good} />
-      <Statistics text="Average" statistics={(good - bad)/ (good + bad + neutral)} />
-      <Statistics text="Positive" statistics={good /(good + bad + neutral) * 100 +'%'} />
+      <div>
+        <Statistics text="Good" statistics={good} />
+        <Statistics text="Neutral" statistics={neutral} />
+        <Statistics text="Bad" statistics={bad} />
+        <Statistics text="Reviews" statistics={bad + neutral + good} />
+        <Statistics text="Average" statistics={average()} />
+        <Statistics
+          text="Positive"
+          statistics={(good / (good + bad + neutral)) * 100}
+          append="%"
+        />
+      </div>
     </div>
   );
 };

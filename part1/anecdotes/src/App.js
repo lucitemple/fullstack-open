3be2d487df6bbde71fa95037477adbdd98671e6d
Luci Temple <lucitemple@yahoo.com.au>
2021-04-20 10:17:51 +1000

    Complete 1.12 anecdotes step 1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -12,7 +12,17 @@ const App = () => {
 
   const [selected, setSelected] = useState(0);
 
-  return <div>{anecdotes[selected]}</div>;
+  const handleClick = () => {
+    let random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random);
+  }
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleClick}>next anecdote</button>
+    </div>
+  );
 };
 
 export default App;

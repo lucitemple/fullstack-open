a8d1282a79bf8a400f3acce1a9d8ba3befba38f4
Luci Temple <lucitemple@yahoo.com.au>
2021-05-17 22:03:51 +1000

    2.8 Add phone numbers

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
 //import { data } from "./data";
 
-const Numbers = ({ newName, persons }) => {
+const Numbers = ({persons }) => {
   return (
     <>
       <h2>Numbers</h2>
       {persons.map((item) => {
-        return <p key={item.name}>{item.name}</p>;
+        return <p key={item.name}>{item.name}: {item.number}</p>;
       })}
     </>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "948990003" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState(0);
 
-  const handleChange = (event) => {
-    setNewName(event.target.value);
+  const handleChange = (func, e) => {
+    func(e.target.value);
   };
 
   const addName = (event) => {
@@ -25,21 +26,28 @@ const App = () => {
     if (persons.some((contact) => contact.name === newName)) {
       alert(`${newName} is already added to the phonebook.`);
     } else {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, number: newNumber }));
     }
     setNewName("");
+    setNewNumber("");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <input value={newName} onChange={handleChange} />
+        <div>
+          name: <input value={newName} onChange={(event) => handleChange(setNewName, event)} />
+        </div>
+        <div>
+          number:
+          <input value={newNumber} onChange={(event) => handleChange(setNewNumber, event)} />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <Numbers newName={newName} persons={persons} />
+      <Numbers persons={persons} />
     </div>
   );
 };

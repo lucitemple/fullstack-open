003e24970c4bee3def8dd8b4feb0dcd0e7d0c57a
Luci Temple <lucitemple@yahoo.com.au>
2021-05-18 18:51:29 +1000

    2.13 show button

--- part2/countries/src/components/ResultsList.js
+++ part2/countries/src/components/ResultsList.js
@@ -1,12 +1,12 @@
 import React from "react";
+import {ShowButton} from "./ShowButton";
 
 export const ResultsList = ({ tenResults }) => {
 
   return (
     <>
       {tenResults.map((country) => {
-        console.log(country.name);
-        return <p key={country.name}>{country.name}</p>;
+        return(<><p key={country.name}>{country.name}</p> <ShowButton key={`${country.name}Button`} /> </>);
       })}
     </>
   );
---
+++ part2/countries/src/components/ShowButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ShowButton = () => {
+    return (
+        <button type="button">show</button>
+    )
+}

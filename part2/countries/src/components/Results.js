a7ad2b6d9de5817d14d1f466aa6cdd4f87d7ccbb
Luci Temple <lucitemple@yahoo.com.au>
2021-05-19 14:16:16 +1000

    2.14 countries weather working

--- part2/countries/src/components/Results.js
+++ part2/countries/src/components/Results.js
@@ -10,15 +10,17 @@ export const Results = ({ results, setResults, weather }) => {
   }
   return (
     <>
-      {results.length < 1 ? (
+      {
+        results.length < 1 ? (
         <p>No matches, please try again</p>
       ) : results.length === 1 ? (
-        <CountryCard result={results[0]} weather={weather} />
+        <CountryCard result={results[0]} />
       ) : results.length > 10 ? (
         <p>Too many matches, please be more specific</p>
       ) : (
         <ResultsList tenResults={tenResults} setResults={setResults} />
-      )}
+      )
+      }
     </>
   );
 };
--- part2/countries/src/util/Weather.js
+++ part2/countries/src/util/Weather.js
@@ -2,26 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { WeatherCard } from "../components/WeatherCard";
 
-const API_KEY = `${process.env.REACT_APP_WEATHERSTACK_API_KEY}`;
-const apiUrl = "http://api.weatherstack.com/current";
+const api_key = `${process.env.REACT_APP_WEATHERSTACK_API_KEY}`;
 
 export const Weather = ({ capital }) => {
   const [weather, setWeather] = useState({});
   const [display, setDisplay] = useState(false);
 
   useEffect(() => {
-    axios
-      .get(
-        `http://api.weatherstack.com/current?access_key=a750d65f415e217ae499dc64f7da87a5&query=New York`
-      )
-      .then((response) => {
-        console.log(response.data);
-        setWeather(response.data);
-        setDisplay(true);
-      });
+    const apiUrl = `http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`;
+
+    axios.get(apiUrl).then((response) => {
+      setWeather(response.data);
+      setDisplay(true);
+    });
   }, [capital]);
 
   if (display) {
     return <WeatherCard weather={weather} />;
-  } else { return null}
+  } else {
+    return null;
+  }
 };

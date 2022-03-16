ed87add96251b26c8cf5f00742b91666cd7b66fd
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2022-03-16 22:13:32 +1100

    change baseurls

--- part3/notes/src/services/notes.js
+++ part3/notes/src/services/notes.js
@@ -2,8 +2,8 @@ import axios from "axios";
 // if running with local json-server, use this:
 // const baseUrl = "http://localhost:3001/api/notes";
 
-// when hosted on heroku, use this:
-const baseUrl = "https://notes-simple-server.herokuapp.com/api/notes";
+// when frontend and backend hosted on heroku, can use relative path:
+const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);

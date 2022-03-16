db1b0490e80497d607e21e3dfd7b0bd65b637c1c
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2022-03-16 20:44:33 +1100

    Add heroku

--- part3/simplewebserver/index.js
+++ part3/simplewebserver/index.js
@@ -77,6 +77,6 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
-console.log(`Server running at http://localhost:${PORT}`);
+console.log(`Server running on port ${PORT}`);

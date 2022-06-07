b48c361cf58c3ab482d9daa7697cb60fc07fdec2
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2022-06-07 21:33:10 +1000

    part3 add build folder to gitignore

--- part3/notes/src/App.js
+++ part3/notes/src/App.js
@@ -36,16 +36,16 @@ function App() {
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
-  
+
     noteService
       .update(id, changedNote)
       .then((returnedNote) => {
-        setNotes(
-          notes.map((note) =>  (note.id !== id ? note : returnedNote)
-        ));
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
       })
       .catch((error) => {
-        setErrorMessage(`Note '${note.content}' could not be found. May have been deleted from the server.`);
+        setErrorMessage(
+          `Note '${note.content}' could not be found. May have been deleted from the server.`
+        );
         setTimeout(() => {
           setErrorMessage(null);
         }, 5000);
